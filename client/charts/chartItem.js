4e5262399299f9765033d6b40b3e3ca9f9edfe01
Matt Gale <matt@littleball.co.uk>
2015-04-14 17:46:13 +0100

    Allowed for adding new data to charts

--- client/charts/chartItem.js
+++ client/charts/chartItem.js
@@ -1,1 +1,31 @@
-Template.chartItem.helpers({});
+Template.chartItem.rendered = function () {
+
+};
+
+Template.chartItem.helpers({
+
+});
+
+Template.chartItem.events({
+
+    'submit form': function (e) {
+        e.preventDefault();
+
+        var $answer = $(e.target).find('[name=answer]');
+
+        var answer = {
+            _id: this._id,
+            value: $answer.val().toLowerCase()
+        };
+
+        Meteor.call('chartAddValue', answer, function (error, result) {
+
+            if (error) {
+                alert(error.reason);
+            }
+        });
+
+        $answer.val('');
+
+    }
+});
