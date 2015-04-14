f536605855673ba238763b5b4c5c1b780832d22f
Matt Gale <matt@littleball.co.uk>
2015-04-14 18:07:14 +0100

    Added svg

--- client/charts/chartItem.js
+++ client/charts/chartItem.js
@@ -1,5 +1,11 @@
 Template.chartItem.rendered = function () {
 
+    var paper = Raphael('paper', 100, 100);
+    var rect = paper.rect(0, 0, 100, 100);
+    rect.attr('fill', '#f00');
+    var circle = paper.circle(50, 50, 30);
+    circle.attr('fill', '#0f0');
+
 };
 
 Template.chartItem.helpers({
